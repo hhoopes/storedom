ba22b1c8a01a7a7951bb8d047ad51e59665035cf
hhoopes <heidih@gmail.com>
2016-04-03 12:29:17 -0600

    Fix conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027155846) do
+ActiveRecord::Schema.define(version: 20160322174754) do
 
   create_table "items", force: true do |t|
     t.string   "name"
